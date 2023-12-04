e90ad394e495d7c79336e5f5b6d75d8ac25e3434
Ragnar Lothbrok <30740511+RagnarLothbrok-Odin@users.noreply.github.com>
2023-12-04 19:21:45 +0000

    Improve error handling in file upload script
    Updated main.py script to include more detailed error messages for unsupported file extensions and files exceeding size limit. Swapped 'import time' for 'from time import sleep' for cleaner code. Added a small delay before attempting to upload a file, to avoid issues with file access permissions. Also incorporated an additional check to verify if the specified path exists, and added specific PermissionError handling.

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 import os.path
-import time
 import requests
 import mimetypes
 import pyperclip
+from time import sleep
 from watchdog.observers import Observer
 from watchdog.events import FileSystemEventHandler
 
@@ -22,33 +22,47 @@ class MonitorFolder(FileSystemEventHandler):
 
         # Return if the file does not have an acceptable file type
         if os.path.splitext(event.src_path)[1] not in valid_extensions:
+            print(f"Error: {os.path.basename(event.src_path)} has an unsupported file extension. "
+                  f"Allowed extensions: {', '.join(valid_extensions)}")
             return
 
         # Return if file size is greater than 40MB
-        if os.path.getsize(event.src_path) >= 40 * 1 << 20:
+        if os.path.getsize(event.src_path) >= 40 * (1 << 20):
+            print(f"Error: {os.path.basename(event.src_path)} exceeds the permitted file size limit "
+                  f"({os.path.getsize(event.src_path) / (1 << 20):.2f}MB > 40MB).")
             return
 
-        headers = {"Authorization": f"{access_token}"}
-        files = {
-            "file": (
-                os.path.basename(event.src_path),
-                open(event.src_path, "rb"),
-                mimetypes.guess_type(event.src_path)[0],
-            )
-        }
-        response = requests.post(api_url, headers=headers, files=files, timeout=10)
+        sleep(0.02)
 
-        if response.status_code == 200:
-            print("File Uploaded Successfully.")
-            # Copy the URL to the clipboard
-            pyperclip.copy(response.json()["files"][0])
-        else:
-            print("Failed to upload file with status code:", response.status_code)
+        try:
+            headers = {"Authorization": f"{access_token}"}
+            files = {
+                "file": (
+                    os.path.basename(event.src_path),
+                    open(event.src_path, "rb"),
+                    mimetypes.guess_type(event.src_path)[0],
+                )
+            }
+            response = requests.post(api_url, headers=headers, files=files, timeout=10)
+
+            if response.status_code == 200:
+                print(f"File Uploaded Successfully: {response.json()['files'][0]}")
+                # Copy the URL to the clipboard
+                pyperclip.copy(response.json()["files"][0])
+            else:
+                print(f"File upload failed. Status code: {response.status_code}")
+        except PermissionError as error:
+            print(error)
+            return
 
 
 if __name__ == "__main__":
     # Absolute path to monitor for new files
     path = "<your-absolute-path>"
+
+    # Check if the path exists
+    if not os.path.exists(path):
+        raise FileNotFoundError(f"The specified path does not exist: {path}")
 
     event_handler = MonitorFolder()
     observer = Observer()
@@ -58,7 +72,7 @@ if __name__ == "__main__":
 
     try:
         while True:
-            time.sleep(1)
+            sleep(1)
     except KeyboardInterrupt:
         observer.stop()
         observer.join()
