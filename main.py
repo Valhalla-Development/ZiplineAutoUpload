74621f8d21b43edb0468957c03dddfc686ff5761
Ragnar Lothbrok <30740511+RagnarLothbrok-Odin@users.noreply.github.com>
2023-12-09 23:53:18 +0000

    🐛 fix(main.py): import specific functions from modules to improve code readability and reduce namespace pollution

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-import mimetypes
-import os.path
+from mimetypes import guess_type
+from os.path import basename, isfile, splitext, getsize, exists
 from time import sleep
 
-import pyperclip
+from pyperclip import copy
 from requests import post
 from watchdog.events import FileSystemEventHandler
 from watchdog.observers import Observer
@@ -25,9 +25,9 @@ def upload_file(path):
         headers = {"Authorization": f"{USER_ACCESS_TOKEN}"}
         files = {
             "file": (
-                os.path.basename(path),
+                basename(path),
                 open(path, "rb"),
-                mimetypes.guess_type(path)[0],
+                guess_type(path)[0],
             )
         }
         response = post(API_UPLOAD_URL, headers=headers, files=files, timeout=10)
@@ -35,7 +35,7 @@ def upload_file(path):
         if response.status_code == 200:
             print(f"File uploaded successfully: {response.json()['files'][0]}")
             # Copy the URL to the clipboard
-            pyperclip.copy(response.json()["files"][0])
+            copy(response.json()["files"][0])
         elif response.status_code == 401:
             print("Authentication failed. Please check your USER_ACCESS_TOKEN.")
         else:
@@ -48,19 +48,19 @@ def upload_file(path):
 class MonitorFolder(FileSystemEventHandler):
     def on_created(self, event):
         # Return if the file is not a valid file
-        if not os.path.isfile(event.src_path):
+        if not isfile(event.src_path):
             return
 
         # Return if the file does not have an acceptable file type
-        if os.path.splitext(event.src_path)[1] not in VALID_EXTENSIONS:
-            print(f"Error: {os.path.basename(event.src_path)} has an unsupported file extension. "
+        if splitext(event.src_path)[1] not in VALID_EXTENSIONS:
+            print(f"Error: {basename(event.src_path)} has an unsupported file extension. "
                   f"Allowed extensions: {', '.join(VALID_EXTENSIONS)}")
             return
 
         # Return if file size is greater than 40MB
-        if os.path.getsize(event.src_path) >= MAX_FILE_SIZE_MB * (1 << 20):
-            print(f"Error: {os.path.basename(event.src_path)} exceeds the permitted file size limit "
-                  f"({os.path.getsize(event.src_path) / (1 << 20):.2f}MB > 40MB).")
+        if getsize(event.src_path) >= MAX_FILE_SIZE_MB * (1 << 20):
+            print(f"Error: {basename(event.src_path)} exceeds the permitted file size limit "
+                  f"({getsize(event.src_path) / (1 << 20):.2f}MB > 40MB).")
             return
 
         sleep(0.02)
@@ -70,7 +70,7 @@ class MonitorFolder(FileSystemEventHandler):
 
 if __name__ == "__main__":
     # Check if the path exists
-    if not os.path.exists(MONITOR_FOLDER_PATH):
+    if not exists(MONITOR_FOLDER_PATH):
         raise FileNotFoundError(f"The specified path does not exist: {MONITOR_FOLDER_PATH}")
 
     event_handler = MonitorFolder()
