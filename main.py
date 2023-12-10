c187fa05ae19e9103982ea80262217022d31bda9
Ragnar Lothbrok <30740511+RagnarLothbrok-Odin@users.noreply.github.com>
2023-12-10 00:12:10 +0000

    🔧 chore(main.py): improve code readability and add docstrings to functions

--- main.py
+++ main.py
@@ -11,16 +11,24 @@ from watchdog.observers import Observer
 MONITOR_FOLDER_PATH = "<path>"
 # List of valid file extensions that will be considered for upload
 VALID_EXTENSIONS = [".png", ".jpg", ".jpeg", ".mov"]
-# URL of the API endpoint for uploading files to your Zipline instance
+# The URL of the API endpoint for uploading files to your Zipline instance
 API_UPLOAD_URL = "https://<domain>/api/upload"
-# Access token associated with your user account for authentication
+# The access token associated with your user account for authentication
 USER_ACCESS_TOKEN = "<access_token>"
-# Maximum allowable size for an individual file in megabytes
+# The maximum allowable size for an individual file, specified in megabytes
 MAX_FILE_SIZE_MB = 40
 
 
 def upload_file(path):
-    # Upload the file to Zipline
+    """
+    Uploads a file to the Zipline API.
+
+    Parameters:
+    - path (str): The absolute path to the file.
+
+    Returns:
+    None
+    """
     try:
         headers = {"Authorization": f"{USER_ACCESS_TOKEN}"}
         files = {
@@ -34,46 +42,57 @@ def upload_file(path):
 
         if response.status_code == 200:
             print(f"File uploaded successfully: {response.json()['files'][0]}")
-            # Copy the URL to the clipboard
-            copy(response.json()["files"][0])
+            copy(response.json()["files"][0])  # Copy the URL to the clipboard
         elif response.status_code == 401:
             print("Authentication failed. Please check your USER_ACCESS_TOKEN.")
         else:
             print(f"File upload failed. Status code: {response.status_code}")
     except PermissionError as error:
         print(error)
-        return
 
 
 def validate_file(path):
-    # Return if the file is not a valid file
+    """
+    Validates a file based on its type and size.
+
+    Parameters:
+    - path (str): The absolute path to the file.
+
+    Returns:
+    None
+    """
     if not isfile(path):
         return
 
-    # Return if the file does not have an acceptable file type
     if splitext(path)[1] not in VALID_EXTENSIONS:
         print(f"Error: {basename(path)} has an unsupported file extension. "
               f"Allowed extensions: {', '.join(VALID_EXTENSIONS)}")
         return
 
-    # Return if file size is greater than 40MB
     if getsize(path) >= MAX_FILE_SIZE_MB * (1 << 20):
         print(f"Error: {basename(path)} exceeds the permitted file size limit "
               f"({getsize(path) / (1 << 20):.2f}MB > 40MB).")
-        return
 
 
 class MonitorFolder(FileSystemEventHandler):
     def on_created(self, event):
-        validate_file(event.src_path)
+        """
+        Event handler for created files in the monitored folder.
 
-        sleep(0.02)
+        Parameters:
+        - event (FileSystemEvent): The event object representing the created file.
 
-        upload_file(event.src_path)
+        Returns:
+        None
+        """
+        validate_file(event.src_path)  # Attempt to validate the file; return if an issue arises
+
+        sleep(0.02)  # Apply a small sleep time to allow for system events
+
+        upload_file(event.src_path)  # Attempt to upload the file
 
 
 if __name__ == "__main__":
-    # Check if the path exists
     if not exists(MONITOR_FOLDER_PATH):
         raise FileNotFoundError(f"The specified path does not exist: {MONITOR_FOLDER_PATH}")
 
